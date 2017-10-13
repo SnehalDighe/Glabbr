cbbc32d5acc4ebf1c2074fd1602110cb170535d0
SnehalDighe <snehaldighe@glabbr.com>
2017-10-13 17:09:16 +0530

    Working with Varibale,loops,instanceof Operator 13 Oct

---
+++ Student.java
@@ -0,0 +1,43 @@
+import java.io.*;
+public class Student 
+{
+//Class/Static Variable
+  private static String address;
+
+ // instance variable 
+   public String name;
+   public int id;
+
+   
+
+  
+void display()
+{
+//Local Variables
+int num1,num2=0;
+}
+
+   
+   public static void main(String args[]) {
+
+     String name = "Snehal";
+
+      // instanceof operator
+      boolean result = name instanceof String;
+      System.out.println( result );
+
+address = "PUNE";
+      System.out.println( "ADDRESS:" + address);
+      Student student = new Student();
+
+//Enhanced For Loop
+int [] tables = {10, 20,30, 40, 50};
+
+      for(int x : tables ) {
+         System.out.print( x );
+         System.out.print("\n");
+      }
+
+      
+   }
+}
